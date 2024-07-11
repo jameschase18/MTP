fae62009d8412b3422a6dafb426fc7ebd370289c
R T <rafal.trzebuniak@gmail.com>
2024-07-11 19:28:11 +0200

    blank project

---
+++ src/test/java/com/example/MyTimePlan/MyTimePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyTimePlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTimePlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
